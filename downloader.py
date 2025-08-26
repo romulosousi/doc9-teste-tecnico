9a50d48c945aca1cc226bb0f0002c3dd3afb888a
romulosousi <romulosouza1738@gmail.com>
2025-08-26 17:13:25 -0300

    Commit completo do Teste Técnico feito pela Doc9. contendo README, main.py, e todos os módulos

---
+++ downloader.py
@@ -0,0 +1,21 @@
+import os
+import requests
+from concurrent.futures import ThreadPoolExecutor
+
+def baixar_fatura(item, base_url, save_dir):
+    invoice_url = base_url + item["invoice"]
+    numero_fatura = os.path.splitext(os.path.basename(item["invoice"]))[0]
+    filename = os.path.join(save_dir, f"{numero_fatura}.jpg")
+
+    r = requests.get(invoice_url, stream=True)
+    r.raise_for_status()  # lança erro se a requisição falhar
+
+    with open(filename, "wb") as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            f.write(chunk)
+
+    print(f"Baixado: {filename}")
+
+def baixar_faturas_paralelo(faturas, base_url, save_dir):
+    with ThreadPoolExecutor(max_workers=12) as executor:
+        executor.map(lambda item: baixar_fatura(item, base_url, save_dir), faturas)
